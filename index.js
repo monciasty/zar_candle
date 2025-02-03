a14ba439132db605acb18c9a7bad47b34093babe
monciasty <117080659+monciasty@users.noreply.github.com>
2025-02-03 20:32:40 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true, // Optional: makes pagination bullets clickable
+  },
   on: {
     slideChange: function() {
       const activeIndex = this.activeIndex;
@@ -15,13 +19,52 @@ var swiper = new Swiper(".mySwiper", {
       
       document.getElementById('headTitle').innerText = slideContent.title;
       document.getElementById('candleDescr').innerText = slideContent.description;
+
+      // Update pagination on slide change
+      this.pagination.update(); // Ensure pagination updates correctly
     }
   }
 });
 
-const input = document.getElementById('candleZ');
-    const displayValue = document.getElementById('currentValue');
+  
+  
+function setupCounter(decrementId, incrementId, inputId) {
+      const adding = document.getElementById(inputId);
+      const decrementButton = document.getElementById(decrementId);
+      const incrementButton = document.getElementById(incrementId);
 
-    input.addEventListener('input', function() {
-        displayValue.innerText = this.value; // Aktualizuje wyświetlaną wartość
-    });
+      decrementButton.addEventListener('click', () => {
+          let currentValue = parseInt(adding.value);
+          if (currentValue > 0) {
+              adding.value = currentValue - 1;
+          }
+
+          decrementButton.style.backgroundColor = '#AC9181';
+          decrementButton.style.color = '#ffffff';
+
+            setTimeout(() => {
+              decrementButton.style.backgroundColor = '#ffffff'; // Pierwotny kolor tła
+              decrementButton.style.color = '#AC9181'; // Pierwotny kolor tekstu
+          }, 100);
+
+      });
+
+      incrementButton.addEventListener('click', () => {
+          let currentValue = parseInt(adding.value);
+          adding.value = currentValue + 1;
+
+          incrementButton.style.backgroundColor = '#AC9181';
+          incrementButton.style.color = '#ffffff';
+          
+          setTimeout(() => {
+            incrementButton.style.backgroundColor = '#ffffff'; // Pierwotny kolor tła
+            incrementButton.style.color = '#AC9181'; // Pierwotny kolor tekstu
+        }, 100);
+
+      });
+  }
+
+  setupCounter('decrementX', 'incrementX', 'candleX');
+  setupCounter('decrementY', 'incrementY', 'candleY');
+  setupCounter('decrementZ', 'incrementZ', 'candleZ');
+  setupCounter('decrementA', 'incrementA', 'candleA');
